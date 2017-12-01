5eacecc76a587015e24dd5c0084a9824a3047de4
Giovanni Alexander <alexander.hh.cc@gmail.com>
2017-12-01 12:13:38 -0500

    Compilando librerias

--- ELF_HuayamaGiovanni/AppDelegate.swift
+++ ELF_HuayamaGiovanni/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import MagicalRecord
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        MagicalRecord.setupCoreDataStack(withStoreNamed: "ELF_HuayamaGiovanni")
         // Override point for customization after application launch.
         return true
     }
